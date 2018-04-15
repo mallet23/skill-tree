75e21210419c0bbbdeb0ed45085beff0350f06c0
Artur Salakhov <gsmilexd@gmail.com>
2018-04-16 01:41:50 +0300

    base entities: skill and base role

---
+++ SkillTree/Roles/BaseRole.cs
@@ -0,0 +1,14 @@
+﻿namespace SkillTree
+{
+    public class Role
+    {
+        public string Name { get; }
+        public Skill[] Skills { get; }
+
+        public Role(string name, Skill[] skills)
+        {
+            Name = name;
+            Skills = skills;
+        }
+    }
+}
